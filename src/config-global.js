92141c58295a8b16a2b4108961571e0925fb0600
farisahmdali <108822848+farisahmdali@users.noreply.github.com>
2025-05-12 13:07:10 +0530

    added phone number

--- src/config-global.js
+++ src/config-global.js
@@ -1,2 +1,2 @@
 export const SERVER_URL = `${import.meta.env.VITE_SERVER_URL}/api/v1`;
-export const ORDER_MOBILENO = `${import.meta.env.VITE_ORDER_MOBILENO}`;
+export const ORDER_MOBILENO = `+917356828384`;
